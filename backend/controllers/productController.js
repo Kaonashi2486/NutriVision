8c59d055d801a1817579c2d991f96a29e9986f0e
boombot11 <sahilsanskruti11@gmail.com>
2025-01-20 10:26:40 +0530

    removed clutter

--- backend/controllers/productController.js
+++
@@ -1,6 +0,0 @@
-const Messages = require("../constants/Message");
-const JsonResponse = require("../helper/JsonResponse");
-const TryCatch = require("../helper/TryCatch");
-const Product = require("../models/Product");
-
-
